61c18bd0d2124adcf51d65818277ef6073bae9e2
RPTED255 <eduardo_garcia@ciencias.unam.mx>
2023-04-26 20:11:57 -0600

    Laberinto 26/04/2023
    Cambios realizados principalmente para los métodos de Laberinto.java

--- src/GenerarLaberinto.java
+++ src/GenerarLaberinto.java
@@ -5,18 +5,23 @@ import java.util.Scanner;
 public class GenerarLaberinto{
 
 	public static void main(String[] args){
+		//La siguiente línea es para la parte de la entrada estándar en terminal que pide en el PDF
+		// public static void main(int ancho, int largo){
 		System.out.println("¡Bienvenid@ al generador de Laberintos!");
 		Scanner sc = new Scanner(System.in);
-		int ancho;
-		int largo;
+
+		// Comentar las 4 líneas siguientes si es necesario por lo de la entrada estándar
 		System.out.println("Introduzca el ancho del tablero: ");
-		ancho = sc.nextInt();
+		int ancho = sc.nextInt();
 		System.out.println("Introduzca el largo del tablero: ");
-		largo = sc.nextInt();
+		int largo = sc.nextInt();
+
+
 		Laberinto l = new Laberinto(ancho, largo);
 		l.generaTablero();
 		l.generaLaberinto();
 		System.out.println(l.printLaberinto());
-	}
 
+		l.generaSolución();
+	}
 }
