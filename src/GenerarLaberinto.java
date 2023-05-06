653af2121f5468043299e29fd6000d37eb0bccdf
Lilith Diaz <lilithdiaz@MacBook-Air-de-Lilith.local>
2023-05-06 17:07:55 -0600

    Solución

--- src/GenerarLaberinto.java
+++ src/GenerarLaberinto.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 
 public class GenerarLaberinto{
 
-	public static void main(int ancho, int largo){
+	public static void main(String[] args){
 
 		System.out.println("¡Bienvenid@ al generador de Laberintos!");
 		Scanner sc = new Scanner(System.in);
 
-		// Comentar las 4 líneas siguientes si es necesario por lo de la entrada estándar
-		/*System.out.println("Introduzca el largo del tablero: ");
+		System.out.println("Introduzca el largo del tablero: ");
 		int largo = sc.nextInt();
 		System.out.println("Introduzca el ancho del tablero: ");
-		int ancho = sc.nextInt();*/
+		int ancho = sc.nextInt();
 
 		Laberinto l = new Laberinto(ancho, largo);
 		l.generaTablero();
 		l.generaLaberinto();
 		System.out.println(l.printLaberinto());
-
+		l.generaSolucion();
+		System.out.println(l.printRespuesta());
 	}
 }
--- src/Laberinto.java
+++ src/Laberinto.java
@@ -1,5 +1,6 @@
 import estructuras.*;
 import java.util.Random;
+import java.util.NoSuchElementException;
 
 public class Laberinto{
 	// Atributos
@@ -133,7 +134,7 @@ public class Laberinto{
 				c.dequeue();
 				try {
 					aux = (Casilla) c.peek();
-				}catch(NullPointerException  a) {break;}
+				}catch(NullPointerException  | NoSuchElementException a) {break;}
 				i = aux.getIndexI();
 				j = aux.getIndexJ();
 			}
@@ -151,76 +152,64 @@ public class Laberinto{
 
 	// Método que genera solución
 	public void generaSolucion(){
-		Pila p = new Pila();
-		int i = generaCoord.nextInt(largo);
-		int j = generaCoord.nextInt(ancho);
-		Casilla aux = tablero[i][j];
-		int copiaInicioI = i, copiaInicioJ = j;
-		int k = generaCoord.nextInt(largo);
-		int l = generaCoord.nextInt(ancho);
-		Casilla fin = tablero[k][l];
-		//System.out.println("Coordenadas del inicio: " + aux.getIndexI() + ", " + aux.getIndexJ());
-		//System.out.println("Coordenadas del final: " + fin.getIndexI() + ", " + fin.getIndexJ());
+		Pila casillasPróximas = new Pila();
+		Pila casillasExploradas = new Pila();
+		Casilla casillaActual = tablero[0][0];
+		Pila i = new Pila();
+		Pila j = new Pila();
+		int iActual = 0;
+		int jActual = 0;
+		int iFinal = largo-1;
+		int jFinal = ancho-1;
 
-		// Marcarla como visitada
-		aux.explorada();
+		casillasPróximas.push(casillaActual);
+		casillasExploradas.push(casillaActual);
+		i.push(iActual);
+		j.push(jActual);
 
-		// Agregarla a la pila
-		aux.explorada();
-		aux.setInicio(true);
-
-		// Agregarla a la pila
-		p.push(aux);
-		while(!p.esVacia()) {
-			if (!(copiaInicioI == fin.getIndexI() && copiaInicioJ == fin.getIndexJ())) {
-				if (aux.vecinos() > 0) {
-					// System.out.println("Coordenadas actuales: " + aux.getIndexI() + ", " + aux.getIndexJ());
-					int indexVecino = encontrarIndice(aux.getVecinosDisponibles());
-					// Corregir el número de vecinos por visitar de la casilla actual
-					aux.vecinosPorVisitar(aux.vecinos() - 1);
-					if (indexVecino == 0 && !tablero[i][j - 1].esExplorada() && aux.getVecinoIzquierdo()) { // El vecino izquierdo está sin visitar
-						// Indicar en el array de la casilla actual que el vecino izquierdo ya está visitado
-						aux.setVecinos(0, false);
-						aux.setVisitados(0, false);
-						// Volver a la casilla actual el vecino izquierdo
-						aux = tablero[i][j - 1];
-					} else if (indexVecino == 1 && !tablero[i - 1][j].esExplorada() && aux.getVecinoSuperior()) { // El vecino superior está sin visitar
-						// Indicar en la array de la casilla actual que el vecino superior ya está visitado
-						aux.setVecinos(1, false);
-						aux.setVisitados(1, false);
-						// Volver a la casilla actual el vecino superior
-						aux = tablero[i - 1][j];
-					} else if (indexVecino == 2 && !tablero[i + 1][j].esExplorada() && aux.getVecinoInferior()) { // El vecino inferior está sin visitar
-						// Indicar en la array de la casilla actual que el vecino inferior ya está visitado
-						aux.setVecinos(2, false);
-						aux.setVisitados(2, false);
-						// Volver a la casilla actual el vecino inferior
-						aux = tablero[i + 1][j];
-					} else if (indexVecino == 3 && !tablero[i][j + 1].esExplorada() && aux.getVecinoDerecho()) { // El vecino derecho está sin visitar
-						// Indicar en la array de la casilla actual que el vecino derecho ya está visitado
-						aux.setVecinos(3, false);
-						aux.setVisitados(3, false);
-						// Volver a la casilla actual el vecino derecho
-						aux = tablero[i][j + 1];
-					}
-					i = aux.getIndexI();
-					j = aux.getIndexJ();
-					// Indicar que la casilla actual está explorada
-					aux.explorada();
-					aux.camino(true);
-					p.push(aux);
-				} else {
-					aux.camino(false);
-					p.pop();
-					try {
-						aux = (Casilla) p.top();
-					} catch (NullPointerException a) {
-						break;
-					}
-					i = aux.getIndexI();
-					j = aux.getIndexJ();
+		while((iActual != iFinal && jActual != jFinal)){
+			try{
+			casillaActual = (Casilla) casillasPróximas.pop();
+			iActual = (int) i.pop();
+			jActual = (int) j.pop();
+			casillaActual.camino(true);
+			// Abajo
+			if(casillaActual.getBordeInferior() == false){
+				if(casillasExploradas.contiene(tablero[iActual+1][jActual]) == false){
+					casillasPróximas.push(tablero[iActual+1][jActual]);
+					casillasExploradas.push(tablero[iActual+1][jActual]);
+					i.push(iActual+1);
+					j.push(jActual);
 				}
-			} else break;
+			}
+			// Arriba
+			if(casillaActual.getBordeSuperior() == false){
+				if(casillasExploradas.contiene(tablero[iActual-1][jActual]) == false){
+					casillasPróximas.push(tablero[iActual-1][jActual]);
+					casillasExploradas.push(tablero[iActual-1][jActual]);
+					i.push(iActual-1);
+					j.push(jActual);
+				}
+			}
+			// Derecha
+			if(casillaActual.getBordeDerecho() == false){
+				if(casillasExploradas.contiene(tablero[iActual][jActual+1]) == false){
+					casillasPróximas.push(tablero[iActual][jActual+1]) ;
+					casillasExploradas.push(tablero[iActual][jActual+1]) ;
+					i.push(iActual);
+					j.push(jActual+1);
+				}
+			}
+			// Izquierda
+			if(casillaActual.getBordeIzquierdo() == false){
+				if(casillasExploradas.contiene(tablero[iActual][jActual-1])  == false){
+					casillasPróximas.push(tablero[iActual][jActual-1]) ;
+					casillasExploradas.push(tablero[iActual][jActual-1]) ;
+					i.push(iActual);
+					j.push(jActual-1);
+				}
+			}
+		} catch(NoSuchElementException e){break;}
 		}
 	}
 
@@ -232,9 +221,77 @@ public class Laberinto{
 
 	// Para imprimir laberinto con respuesta
 	public String printRespuesta(){
-		setResuelto( generado );
-
+		if(resuelto == null) setResuelto( laberintoRespuesta() );
 		return resuelto;
+	}
+
+	public String laberintoRespuesta(){
+		String lab = "";
+		// Añadir lineas de arriba
+		for(int l = 0; l < ancho+1; l++){
+			lab += "__";
+		}
+		lab += "\n";
+
+		// Recorrer tablero
+		for(int i = 0; i < largo; i++){
+			lab += "|";
+			for(int j = 0; j < ancho; j++){
+				if(i == 0){ // Caso especial de la primera fila
+					if(j == ancho-1) continue;
+					if(tablero[0][j].esCamino()){
+						lab += "x";
+					} else {
+						if(tablero[0][j].getBordeInferior() && tablero[1][j].getBordeSuperior()) lab += "_";
+						else lab += " ";
+					}
+					if(tablero[0][j].getBordeDerecho() && tablero[0][j+1].getBordeIzquierdo()) lab += "|";
+					else lab += " ";
+					if( j == ancho - 2){
+						if(tablero[0][ancho-2].getBordeDerecho() && tablero[0][ancho-1].getBordeIzquierdo()) lab += "|";
+						else lab += " ";
+					}
+				} else if(i == largo-1) { // Caso especial de la última fila
+					if(tablero[i][j].esCamino()){lab += "x";} else {lab += "_";}
+					if(j == ancho-1) continue;
+					if(tablero[largo-1][j].getBordeDerecho() && tablero[largo-1][j+1].getBordeIzquierdo()) lab += "|";
+					else lab += " ";
+				} else if(j == 0) { // Caso especial de la primera columna
+					if(i == 0 && j == 0){
+						if(tablero[0][0].esCamino()){lab += "x";} else {
+							if(tablero[0][0].getBordeInferior() && tablero[1][0].getBordeSuperior()) lab += "_";
+							else lab += " ";
+						}
+						if(tablero[0][0].getBordeDerecho() && tablero[0][1].getBordeIzquierdo()) lab += "|";
+						else lab += " ";
+					} else if(i == largo-1 && j == 0){
+						if(tablero[largo-1][0].esCamino()){lab += "x";} else {lab += "_";}
+					} else{
+						if(tablero[i][0].esCamino()){lab += "x";} else {
+							if(tablero[i][0].getBordeInferior() && tablero[i+1][1].getBordeSuperior()) lab += "_";
+							else lab += " ";
+						}
+						if(tablero[i][0].getBordeDerecho() && tablero[i][1].getBordeIzquierdo()) lab += "|";
+						else lab += " ";
+					}
+				} else if(j == ancho-1) { // Caso especial de la última columna
+					if(tablero[i][ancho-1].esCamino()){lab += "x";} else {
+						if(tablero[i][ancho-1].getBordeInferior() && tablero[i+1][ancho-1].getBordeSuperior()) lab += "_";
+						else lab += " ";
+					}
+				} else {
+					if(i == 0 || i == largo-1 || j == 0 || j == ancho-1) continue;
+					if(tablero[i][j].esCamino()){lab += "x";} else {
+						if(tablero[i][j].getBordeInferior() && tablero[i+1][j].getBordeSuperior()) lab += "_";
+						else lab += " ";
+					}
+					if(tablero[i][j].getBordeDerecho() && tablero[i][j+1].getBordeIzquierdo()) lab += "|";
+					else lab += " ";
+				}
+			}
+			lab += "|\n";
+		}
+		return lab;
 	}
 
 	public String laberintoGrafico(){
